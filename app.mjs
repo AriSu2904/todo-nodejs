443a044a95c57104f4772eed5099e7e7be63e1d9
Ari Susanto <arisusanto290401@gmail.com>
2023-09-30 17:19:16 +0700

    Add Todo

--- app.mjs
+++ app.mjs
@@ -2,10 +2,14 @@ import http from "http"
 import {TodolistService} from "./todolist-service.mjs";
 
 
+const service = new TodolistService()
 const server = http.createServer((request, response) => {
-    const service = new TodolistService()
+    response.setHeader("Content-Type", "application/json")
+
     if(request.method === "GET"){
         service.getTodoList(request, response)
+    }else if(request.method === "POST"){
+        service.addTodoList(request, response)
     }
 })
 
--- todolist-service.mjs
+++ todolist-service.mjs
@@ -1,8 +1,34 @@
 export class TodolistService {
-    todolist = []
+    todolist = ["Belajar", "Todolist", "NodeJS"]
+
+    getJsonTodoList() {
+        return JSON.stringify({
+            code: 200,
+            status: "OK",
+            data: this.todolist.map((value, index) => {
+                return {
+                    id: (index + 1),
+                    todo: value
+                }
+            })
+        })
+    }
+
+
 
     getTodoList(req,res){
+        res.write(this.getJsonTodoList())
+        res.end()
+    }
 
+    addTodoList(req, res) {
+        req.addListener("data", (data) => {
+            const body = JSON.parse(data.toString())
+            this.todolist.push(body)
+        })
+
+        res.write(this.getJsonTodoList())
+        res.end()
     }
 
 }
